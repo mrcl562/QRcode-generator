dc75034afc91b2f497bdbf95e56f31364afdaf62
Marcelo Llanos Maldonado <mrcl562@gmail.com>
2023-01-30 10:45:13 -0400

    generate QR pressing Enter key

--- QRgenerator.js
+++ QRgenerator.js
@@ -2,7 +2,18 @@ let imgContainer = document.querySelector(".imgContainer");
 let qrImage = document.querySelector(".qrImage");
 let qrText = document.querySelector(".qrText");
 
+document.addEventListener("keypress", (ev)=>{
+    ev.key === "Enter" ? generateQR() : false;
+})
+
 const generateQR = () =>{
-    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}` ;
-    imgContainer.classList.add("showImg");
+    if(qrText.value.length > 0){
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+        imgContainer.classList.add("showImg");
+    }else{
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error")
+        }, 1000)
+    }
 }
